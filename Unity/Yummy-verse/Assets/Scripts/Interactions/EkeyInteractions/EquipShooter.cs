3c9a6781276e4e324df0d77a6e1ef89bb4ce9e7a
FrancescoRisso <francesco@frisso.net>
2025-01-31 15:11:14 +0100

    Dismount dallo spara acido

--- Unity/Yummy-verse/Assets/Scripts/Interactions/EkeyInteractions/EquipShooter.cs
+++ Unity/Yummy-verse/Assets/Scripts/Interactions/EkeyInteractions/EquipShooter.cs
@@ -6,7 +6,8 @@ public class EquipShooter : EkeyInteractable {
 
 	private CameraEnabler _my_camera_enabler;
 
-	// Start is called before the first frame update
+	private ShooterInteractionManager _shooter_interaction_manager;
+
 	void Start() {
 		GameObject player = GameObject.FindGameObjectWithTag("Player");
 		Assert.IsNotNull(player, $"{name} cannot find the player");
@@ -16,10 +17,14 @@ public class EquipShooter : EkeyInteractable {
 
 		_my_camera_enabler = GetComponentInChildren<CameraEnabler>();
 		Assert.IsNotNull(_my_camera_enabler, $"{name} cannot find its own camera enabler");
+
+		_shooter_interaction_manager = GetComponentInChildren<ShooterInteractionManager>();
+		Assert.IsNotNull(_shooter_interaction_manager, $"{name} cannot find its shooter interaction manager");
 	}
 
 	public override void StartProcessing() {
 		_player_camera_enabler.Disable();
 		_my_camera_enabler.Enable();
+		_shooter_interaction_manager.enabled = true;
 	}
 }
