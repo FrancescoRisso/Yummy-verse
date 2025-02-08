036bdf937f667cbb3a9f2fc19dd1ff5eaa6be360
FrancescoRisso <francesco@frisso.net>
2025-02-08 15:27:19 +0100

    Fix spara acido rotta

--- Unity/Yummy-verse/Assets/Scripts/Movement/MovementInteractionEnabler.cs
+++ Unity/Yummy-verse/Assets/Scripts/Movement/MovementInteractionEnabler.cs
@@ -15,20 +15,19 @@ public class MovementInteractionEnabler : MonoBehaviour {
 
 		Assert.IsNotNull(_yaw_controller, $"{name} cannot find the yaw controller");
 		Assert.IsNotNull(_pitch_controller, $"{name} cannot find the pitch controller");
-		Assert.IsNotNull(_movement, $"{name} cannot find the movement controller");
 	}
 
 	public void Enable() {
 		_yaw_controller.enabled = true;
 		_pitch_controller.enabled = true;
 		if(_mouse_interact) _mouse_interact.enabled = true;
-		_movement.enabled = true;
+		if(_movement) _movement.enabled = true;
 	}
 
 	public void Disable() {
 		_yaw_controller.enabled = false;
 		_pitch_controller.enabled = false;
 		if(_mouse_interact) _mouse_interact.enabled = false;
-		_movement.enabled = false;
+		if(_movement) _movement.enabled = false;
 	}
 }
